515ce6fc7344d42a6e6677295951ac083b9b0ce8
emrekandazoglu <114920764+emrekandazoglu@users.noreply.github.com>
2023-08-07 18:34:42 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xg": "1140px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
